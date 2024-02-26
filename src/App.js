08c43bed6670fc046d5162777389ac44c0459134
eabra <emab21@student.bth.se>
2024-02-26 14:22:37 +0100

    Finished App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Pricing from "./pages/Pricing";
+import './styles.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
+
   return (
-    "Hello World"
-    );
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
